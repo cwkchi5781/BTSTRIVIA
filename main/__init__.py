5b625a5930d1a59ebba0f55411b7a67e339ba473
cwkchi5781 <friendsrock2004chi@gmail.com>
2020-03-17 19:20:42 -0700

    changed a little bit

--- main/__init__.py
+++ main/__init__.py
@@ -2,15 +2,16 @@ import flask, os, mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 app = flask.Flask(__name__)
 
-SECRET_KEY = os.urandom(32)
-app.secret_key = SECRET_KEY
+#SECRET_KEY = os.urandom(32)
+app.secret_key = "abcdefg"
 
 db = mysql.connector.connect(
     host="104.168.136.69",
     user="yscbtlcv_cwkchi5781",
-    password="singlebird5781!",
+    passwd="singlebird5781!",
     database="BTSTRIVIA"
 )
+
 
 cursor = db.cursor()
 
--- main/routes.py
+++ main/routes.py
@@ -2,7 +2,7 @@ from flask import url_for, render_template, flash, redirect, request
 from main import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
